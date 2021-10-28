3653be7cb97a7423db7d65fbdf56dcbb917248b8
Donato Tulimieri <53941115+Donato-Tuli@users.noreply.github.com>
2021-10-28 17:17:30 +0200

    Delete ROBOT/TESLA/TESLA V0.2 directory

--- ROBOT/TESLA/TESLA V0.2/AVANTI.ino
+++
@@ -1,7 +0,0 @@
-void AVANTI(float W1 ,float W2){
-  analogWrite(pwm_dx,H+W1+80);
-  analogWrite(pwm_sx,H+W2+9);
-  digitalWrite(avantidx,1);
-  digitalWrite(dietrodx,0);
-  digitalWrite(avantisx,1);
-  digitalWrite(dietrosx,0);} 
--- ROBOT/TESLA/TESLA V0.2/RUOTA_DX.ino
+++
@@ -1,8 +0,0 @@
- void ruotadx(float D1 ,float D2){
-  analogWrite(pwm_dx,H+D1+80);
-  analogWrite(pwm_sx,H+D2+9);
-  digitalWrite(avantidx,0);
-  digitalWrite(dietrodx,1);
-  digitalWrite(avantisx,1);
-  digitalWrite(dietrosx,0);
- }
