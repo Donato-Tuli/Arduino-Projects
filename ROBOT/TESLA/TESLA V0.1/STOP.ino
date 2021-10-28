0310bde3ce80f21c05635368044a5081cbae4285
Donato Tulimieri <53941115+Donato-Tuli@users.noreply.github.com>
2021-10-28 17:17:20 +0200

    Delete ROBOT/TESLA/TESLA V0.1 directory

--- ROBOT/TESLA/TESLA V0.1/STOP.ino
+++
@@ -1,8 +0,0 @@
-void STOP() {
-  analogWrite(pwm_dx,0);
-  analogWrite(pwm_sx,0);
-  digitalWrite(avantidx,0);
-  digitalWrite(dietrodx,0);
-  digitalWrite(avantisx,0);
-  digitalWrite(dietrosx,0);
-}
